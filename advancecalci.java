39e22761cd7f668ca70b0c22e60dc10753709b76
Shrinivas <shrinivasvader@gmail.com>
2022-06-21 11:28:49 +0530

    new code

---
+++ advancecalci.java
@@ -0,0 +1,16 @@
+package git;
+
+import java.util.Scanner;
+
+public class advancecalci {
+
+	public static void main(String[] args) {
+		System.out.println("Enter any number");
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		
+		int c=a*a;
+		
+		System.out.println("Square of "+a+" is "+c);
+	}
+}
