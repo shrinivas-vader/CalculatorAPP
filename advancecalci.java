ed820c8f1716f4720a78dd488d1bb0bd6327e899
Shrinivas <shrinivasvader@gmail.com>
2022-06-21 11:40:25 +0530

    new updated code

--- advancecalci.java
+++ advancecalci.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class advancecalci {
 
 	public static void main(String[] args) {
+		
+		System.out.println("This is new change");
+		
 		System.out.println("Enter any number");
 		Scanner sc=new Scanner(System.in);
 		int a=sc.nextInt();
